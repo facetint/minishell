82800cdb3926e2803a6b72e742e7d96fc37e131c
hamza <hamzacoskun41@gmail.com>
2023-12-30 14:33:22 +0300

    feat: add functions to expand environment variables.

--- expander.c
+++ expander.c
@@ -1,33 +1,76 @@
 #include "parser.h"
 #include "utils.h"
 #include "libft/libft.h"
-#include <stdio.h>
 
-t_token *expand_lexer_data(char *input, t_token *lexer_data)
+/**
+ * replaces the string at input with replacement.
+ *
+ * @param input input string
+ * @param p_start start index of the string to replace (placeholder start index)
+ * @param p_len length of the string to replace (placeholder length)
+ * @param replacement replacement string
+ */
+char *replace_string(char *input, int p_start, int p_len, char *replacement)
+{
+	char *head;
+	char *tail;
+	char *result;
+
+	head = ft_substr(input, 0, p_start);
+	tail = ft_substr(input, p_start + p_len + 1, ft_strlen(input) - p_start - p_len);
+	result = ft_str_arr_join((char *[]) {head, replacement, tail}, 3);
+	free(head);
+	free(tail);
+	return result;
+}
+
+/**
+ * expands the variable at index in input.
+ *
+ * Example:
+ * $USER = "bsq"
+ * input = "hello $USER"
+ * index = 6
+ * returns -> 8
+ *
+ * @param input input string
+ * @param index index of the variable (must point to '$')
+ * @return length of string before variable + expanded variable length - 1
+ * this return value points to where you should continue for next variable.
+ */
+int expand_variable(char **input, int index)
+{
+	char    *str;
+	char    *varname;
+	char    *new;
+	int        result;
+	int        varname_len;
+
+	str = *input;
+	varname_len = count_len(&str[index + 1], is_name_char2);
+	varname = ft_substr(str, index + 1, varname_len);
+	new = replace_string(str, index, varname_len, getenv(varname));
+	result = (int) ft_strlen(str);
+	*input = new;
+	free(varname);
+	free(str);
+	return index + varname_len + (int) ft_strlen(new) - result;
+}
+
+void expand_all_variables(char **string)
 {
 	int index;
-	int length;
+	char *str;
 
-	while (lexer_data)
+	str = *string;
+	index = 0;
+	while (str[index])
 	{
-		if (lexer_data->type == WORD)
-		{
-			index = lexer_data->start;
-			while (input[index])
-			{
-				if (input[index] == '$' && !is_in_single_quote(input, index)) {
-					length = count_len(&input[index + 1], is_name_char2);
-					char *name = ft_substr(input, index + 1, length);
-					char *expanded = getenv(name);
-					printf("'%s' expanded to '%s'\n", name, expanded);
-					index += length;
-				}
-				index++;
-			}
-		}
-		lexer_data = lexer_data->next;
+		if (str[index] == '$')
+			index = expand_variable(&str, index);
+		index++;
 	}
-	return NULL;
+	*string = str;
 }
 
 /**
