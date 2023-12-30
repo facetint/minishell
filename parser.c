b33ac7f9304b23f6119fdae8a66f22f5f05867f6
hamza <hamzacoskun41@gmail.com>
2023-12-30 14:26:05 +0300

    feat: add parser with finite state machine

--- parser.c
+++ parser.c
@@ -1,234 +1,161 @@
 #include "libft/libft.h"
 #include "parser.h"
-#include <stdio.h>
 #include "utils.h"
 
-/**
- * get token type from meta character
- * @param input the input string
- * @return the token type
- */
-t_token_type get_meta_token_type(const char *input) {
-	if (!input || !*input)
-		return UNKNOWN;
-	if (input[0] == '>') {
-		if (input[1] == '>')
-			return APPEND_REDIRECTION;
-		return OUTPUT_REDIRECTION;
+void join_all_composed_words(t_token **cur_token, char **string_ptr) {
+	char *new_name;
+	t_token *lexer_data;
+
+	lexer_data = *cur_token;
+	if (lexer_data->type == DELIMITER)
+		lexer_data = lexer_data->next;
+	while (lexer_data && is_word(lexer_data->type))
+	{
+		new_name = ft_strjoin(*string_ptr, lexer_data->value);
+		if (*string_ptr)
+			free(*string_ptr);
+		*string_ptr = new_name;
+		lexer_data = lexer_data->next;
 	}
-	if (input[0] == '<') {
-		if (input[1] == '<')
-			return HEREDOC_REDIRECTION;
-		return INPUT_REDIRECTION;
-	}
-	if (input[0] == '|')
-		return PIPE;
-	return UNKNOWN;
+	*cur_token = lexer_data;
 }
 
-/**
- * state for command name
- *
- * Example:
- * 		cmd1 arg1
- * 		^
- * 		cmd1 arg1 | cmd2
- * 		^			^
- * 		cmd1 | cmd2
- * 		^      ^
- * @param lexer_data the lexer data to append new token
- * @param input the input string
- * @param index the index of the first char of the command name
- * @return the next state
- */
-state command_state(t_token **lexer_data, char *input, int index)
-{
-	//assume input and lexer_data is non-null
-	//assume 0 < index < strlen(input)
-	t_token token;
-	token.start = index;
-	while (input[index] && is_name_char(input[index]))
-		index++;
-	if (index == token.start)
-		return unexpected_token_error(input[index]), NULL;
-	token.end = index - 1;
-	token.type = COMMAND;
-	lexer_data_append(lexer_data, lexer_data_new(token));
-	return (state) command_after_state;
-}
+parser_state command_state(t_token **cur_token, t_command **cur_cmd) {
+	t_token *lexer_data;
+	t_command *cmd;
 
-/**
- * state for everything after command name
- *
- * Example:
- * 		cmd1 arg1
- * 			 ^
-* 		cmd1 arg1 | cmd2
- * 			 ^
- * 		cmd1 | cmd2
- * 			 ^
- * 		cmd1 < infile
- * 			 ^
- *
- * @param lexer_data the lexer data to append new token
- * @param input the input string
- * @param index the index of the first char that is not whitespace after command name.
- * @return the next state
- */
-state command_after_state(t_token **lexer_data, char *input, int index)
-{
-	if (input[index] == '\0')
+	cmd = *cur_cmd;
+	lexer_data = *cur_token;
+	join_all_composed_words(&lexer_data, &cmd->name);
+	if (!lexer_data)
 		return NULL;
-	if (is_meta_char(input[index]))
-		return (state) delimiter_state;
-	return (state) argument_state;
+	if (lexer_data->type == DELIMITER)
+		lexer_data = lexer_data->next;
+	*cur_token = lexer_data;
+	if (is_word(lexer_data->type))
+		return (parser_state) argument_state;
+	else if (is_operator(lexer_data->type))
+		return (parser_state) operator_state;
+	return unexpected_token_error(lexer_data), NULL;
 }
 
-/**
- * state for arguments
- *
- * Example:
- * 		cmd1 arg1
- * 			 ^
- * 		cmd1 "arg1"
- * 			 ^
- * 		cmd1 'arg1'
- * 			 ^
- * 		cmd1 -option
- * 			 ^
- *
- * @param lexer_data the lexer data to append new token
- * @param input the input string
- * @param index the index of the first char of the argument (included quotes)
- * @return the next state
- */
-state argument_state(t_token **lexer_data, char *input, int index) {
-	//assume input and lexer_data is non-null
-	//assume 0 < index < strlen(input) 
-	//assume input[index] is not a null, whitespace or metacharacter.
-	t_token token;
-	token.start = index;
-	if (input[index] == DOUBLE_QUOTE)
-	{
-		token.type = DOUBLE_QUOTED_ARGUMENT;
-		index += find_char(&input[index + 1], DOUBLE_QUOTE) + 1;
-	}
-	else if (input[index] == SINGLE_QUOTE)
-	{
-		token.type = SINGLE_QUOTED_ARGUMENT;
-		index += find_char(&input[index + 1], SINGLE_QUOTE) + 1;
-	}
-	else
-	{
-		token.type = UNQUOTED_ARGUMENT;
-		while (input[index] != '\0' && is_name_char(input[index]))
-			index++;
-		index--;
-	}
-	token.end = index;
-	lexer_data_append(lexer_data, lexer_data_new(token));
-	return (state) command_after_state;
-}
-
-/**
- * state for metacharacters
- * Example:
- * 		cmd arg1 | cmd2
- * 				 ^
- *		cmd arg1 > outfile
- *			 	 ^
- *		cmd < infile
- *			^
- *		cmd << heredoc
- *			^
- *		cmd arg1 >> outfile
- *		 		 ^
- * @param lexer_data the lexer data to append new token
- * @param input the input string
- * @param index the index of the first char of the metacharacter
- * @return the next state
- */
-state delimiter_state(t_token **lexer_data, char *input, int index)
+parser_state argument_state(t_token **cur_token, t_command **cur_cmd)
 {
-	t_token_type type;
-	int length;
-	t_token token;
+	t_token *lexer_data;
+	int arg_index;
+	t_command *cmd;
 
-	type = get_meta_token_type(&input[index]);
-	if (type == PIPE || type == OUTPUT_REDIRECTION || type == INPUT_REDIRECTION)
-		length = 1;
-	else if (type == APPEND_REDIRECTION || type == HEREDOC_REDIRECTION)
-		length = 2;
-	else
-		return NULL; //do not append new token if command name not found.
-	token.start = index;
-	token.end = index + length - 1;
-	token.type = type;
-	lexer_data_append(lexer_data, lexer_data_new(token));
-	if (type == PIPE)
-		return (state) command_state;
-	return (state) redirection_word_state;
+	cmd = *cur_cmd;
+	lexer_data = *cur_token;
+	arg_index = str_arr_size(cmd->args);
+	join_all_composed_words(&lexer_data, &cmd->args[arg_index]);
+	if (!lexer_data)
+		return NULL;
+	if (lexer_data->type == DELIMITER)
+		lexer_data = lexer_data->next;
+	*cur_token = lexer_data;
+	if (is_word(lexer_data->type))
+		return (parser_state) argument_state;
+	if (is_operator(lexer_data->type))
+		return (parser_state) operator_state;
+	return unexpected_token_error(lexer_data), NULL;
 }
 
-/**
- *
- * state for words after redirection
- * Example:
- * 		cmd arg1 > outfile arg2
- *                 ^
- *      cmd < infile arg1
- *      	  ^
- *      cmd << heredoc
- *      	   ^
- *     	cmd arg1 >> outfile arg2
- *     			    ^
- *
- * @param lexer_data the lexer data to append new token
- * @param input the input string
- * @param index the index of the first char of the word
- * @return the next state
- */
-state redirection_word_state(t_token **lexer_data, char *input, int index)
+parser_state operator_state(t_token **cur_token, t_command **cur_cmd)
 {
-	t_token token;
-	token.start = index;
-	token.type = WORD;
-	if (is_quote(input[index])) {
-		index += find_char(&input[index + 1], input[index]) + 1;
-	} else {
-		while (input[index] && is_name_char(input[index]))
-			index++;
-		index--;
+	t_token *lexer_data;
+	t_redirection redirection;
+	int last_index;
+
+	lexer_data = *cur_token;
+	if (lexer_data->type == PIPE)
+	{
+		*cur_cmd = (*cur_cmd)->next;
+		*cur_token = (*cur_token)->next;
+		return (parser_state) command_state;
 	}
-	token.end = index;
-	if (token.end < token.start)
-		return unexpected_token_error(input[index]), NULL;
-	lexer_data_append(lexer_data, lexer_data_new(token));
-	return (state) command_after_state;
+	last_index = 0;
+	while ((*cur_cmd)->redirections[last_index].redirected)
+		last_index++;
+	redirection = (t_redirection){.redirected = NULL, .flags = 0};
+	if (lexer_data->type == INPUT_REDIRECTION)
+		redirection.flags = INPUT;
+	else if (lexer_data->type == HEREDOC_REDIRECTION)
+		redirection.flags = HEREDOC | INPUT;
+	else if (lexer_data->type == APPEND_REDIRECTION)
+		redirection.flags = APPEND;
+	lexer_data = lexer_data->next;
+	join_all_composed_words(&lexer_data, &redirection.redirected);
+	(*cur_cmd)->redirections[last_index] = redirection;
+	*cur_token = lexer_data;
+	if (!lexer_data)
+		return NULL;
+	if (lexer_data->type == DELIMITER)
+		lexer_data = lexer_data->next;
+	if (is_word(lexer_data->type))
+		return (parser_state) argument_state;
+	if (is_operator(lexer_data->type))
+		return (parser_state) operator_state;
+	return unexpected_token_error(lexer_data), NULL;
 }
 
-/*
- * get end index of last token
- * (index of last char of scanned part of input)
- */
-int get_index(t_token *token) {
-	t_token *last_token = NULL;
-	last_token = get_last_lexer_data(token);
-	if (!last_token)
-		return 0;
-	return last_token->end + 1;
+int count_cmd_args(t_token *lexer_data) {
+	int delimiter_count;
+
+	delimiter_count = 0;
+	while (lexer_data && lexer_data->type != PIPE)
+	{
+		if (lexer_data->type == DELIMITER)
+			delimiter_count++;
+		lexer_data = lexer_data->next;
+	}
+
+	return delimiter_count;
 }
 
-t_token *lexer(char *input) {
-	t_token *token = NULL;
-	state next_state = (state) command_state;
-	int index;
-	while (next_state) {
-		index = get_index(token);
-		index += skip_white_spaces(&input[index]);
-		if (input[index] == '\0')
-			break;
-		next_state = (state) next_state(&token, input, index);
+int count_cmd_redirections(t_token *lexer_data) {
+	int redirection_count;
+
+	redirection_count = 0;
+	while (lexer_data && lexer_data->type != PIPE)
+	{
+		if (lexer_data->type == INPUT_REDIRECTION
+			|| lexer_data->type == HEREDOC_REDIRECTION
+			|| lexer_data->type == OUTPUT_REDIRECTION
+			|| lexer_data->type == APPEND_REDIRECTION)
+			redirection_count++;
+		lexer_data = lexer_data->next;
 	}
-	return token;
+	return redirection_count;
+}
+
+void init_command(t_token *lexer_data, t_command **command) {
+	int arg_count;
+	int redir_count;
+
+	arg_count = count_cmd_args(lexer_data);
+	redir_count = count_cmd_redirections(lexer_data);
+	(*command) = ft_calloc(sizeof(t_command), 1);
+	(*command)->args = ft_calloc(sizeof(char *), arg_count + 1);
+	(*command)->redirections = ft_calloc(sizeof(t_redirection), redir_count + 1);
+}
+
+t_command *parse(t_token *lexer_data) {
+	t_command *cur_cmd;
+	parser_state next_state;
+	t_command *result;
+
+	/* while (start)*/
+	t_token *cur_token = lexer_data;
+	next_state = (parser_state) command_state;
+	init_command(lexer_data, &cur_cmd);
+	result = cur_cmd;
+	while (next_state)
+	{
+		next_state = (parser_state) next_state(&cur_token, &cur_cmd);
+		if (next_state == (parser_state) command_state)
+			init_command(lexer_data, &cur_cmd);
+	}
+	/* while (end)*/
+	return result;
 }
