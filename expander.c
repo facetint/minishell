e4ba5dd35d0c5c6f2d8d889bddab2471746ababa
hamza <hamzacoskun41@gmail.com>
2023-12-30 14:34:12 +0300

    feat: add the expander function.

--- expander.c
+++ expander.c
@@ -73,42 +73,24 @@ void expand_all_variables(char **string)
 	*string = str;
 }
 
-/**
- * checks is there backslash at the preceding index
- * @param input the input string
- * @param index the index of the char to check
- * @return 1 if there is backslash at the preceding index, 0 otherwise. (if index is 0, return 0.)
- */
-int is_escaped(char *input, unsigned int index) {
-	if (index == 0)
-		return 0;
-	return input[index - 1] == '\\';
-}
 
-/**
- * detects quoted parts of the input and checks
- * the char at index is in a single-quoted part or not.
- * @param input the input string
- * @param index the index of the char to check
- * @return 1 if the char at index is in a single-quoted part, 0 otherwise
- */
-int is_in_single_quote(char *input, int index) {
-	unsigned int i = 0;
-	int found_char_distance;
-	char quote;
-	while (i < index)
+
+void expand(t_token **head)
+{
+	t_token *token;
+
+	token = *head;
+	while (token)
 	{
-		if (is_quote(input[i]) && !is_escaped(input, i))
+		/* only unquoted word and double-quoted word tokens are expandable. */
+		if (token->type == UNQUOTED_WORD || token->type == DOUBLE_QUOTED_WORD)
 		{
-			quote = input[i];
-			found_char_distance = find_char(&input[i + 1], quote);
-			if (found_char_distance == -1)
-				return 0;
-			i += found_char_distance + 1;
-			if (i + 1 >= index && quote == SINGLE_QUOTE)
-				return 1;
+			expand_all_variables(&token->value);
+			/* only unquoted words are not protected for the split */
+			if (token->type == UNQUOTED_WORD)
+				internal_field_split(head, token);
 		}
-		i++;
+		token = token->next;
 	}
 	return 0;
 }
