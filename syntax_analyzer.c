61f89b57f7dea3fe83dd538d37012937828d4faf
hamza <hamzacoskun41@gmail.com>
2024-01-01 01:20:42 +0300

    refact: rename unknown token validation func.

--- syntax_analyzer.c
+++ syntax_analyzer.c
@@ -71,18 +71,21 @@ int are_quotes_valid(t_token *lexer_data)
 	return (1);
 }
 
-int is_there_unknown_token(t_token *lexer_data) {
+int are_tokens_valid(t_token *lexer_data)
+{
 	while (lexer_data)
 	{
 		if (lexer_data->type == UNKNOWN)
-			return 1;
+			return 0;
+		if (is_word(lexer_data->type) && lexer_data->value == NULL)
+			return 0;
 		lexer_data = lexer_data->next;
 	}
-	return 0;
+	return 1;
 }
 
 int is_valid(t_token *lexer_data) {
-	return !is_there_unknown_token(lexer_data)
+	return are_tokens_valid(lexer_data)
 		   && are_quotes_valid(lexer_data)
 		   && is_there_lack_of_word(lexer_data);
 }
