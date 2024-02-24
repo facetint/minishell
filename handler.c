fded6b96971b0c410330c297306c19044270faae
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-02-24 15:43:10 +0300

    chore: implement execute

--- handler.c
+++ handler.c
@@ -139,6 +139,7 @@ void handle_input(char *input)
 	parser_data = parse(lexer_data);
 	handle_heredocs(parser_data);
 
+	execute(parser_data, parser_data);
 	debug(lexer_data, parser_data);
 	uninit_tokens(lexer_data);
 }
