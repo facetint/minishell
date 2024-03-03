c0095a7ab41de717f1afde7a9ac0bf8b2b5d05f9
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-03 14:31:29 +0300

    still: organisation of functions

--- execute/execute.c
+++ execute/execute.c
@@ -6,21 +6,26 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:34:07 by facetint          #+#    #+#             */
-/*   Updated: 2024/02/29 16:42:04 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/03 11:35:26 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/minishell.h"
+#include "../includes/minishell.h"
 #include "unistd.h"
 #include "stdio.h"
 #include <sys/wait.h>
 #include <stdlib.h>
-#include "libft/libft.h"
-#include "memory-allocator/allocator.h"
-#include "includes/env.h"
+#include "../libft/libft.h"
+#include "../memory-allocator/allocator.h"
+#include "../get_next_line/get_next_line.h"
+#include "../includes/env.h"
 
-extern t_envList *envList;
+t_envList *get_global_env()
+{
+    static t_envList env;
 
+    return (&env);
+}
 t_command *find_before_cmd(t_command *cmds, t_command *beginCmds)
 {
     if (cmds == beginCmds)
@@ -28,6 +33,15 @@ t_command *find_before_cmd(t_command *cmds, t_command *beginCmds)
     while (beginCmds->next != cmds) 
         beginCmds = beginCmds->next;
     return (beginCmds);
+}
+
+void read_and_print(int fd) {
+    char *line;
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        write(1, line, ft_strlen(line));
+        safe_free(line);
+    }
 }
 
 char **new_arr(char *new, char **arr)
@@ -47,49 +61,6 @@ char **new_arr(char *new, char **arr)
     return (ret_val);
 }
 
-char *find_path(char *cmd)
-{
-    char *path;
-    char **path_arr;
-    char *joined_path;
-    char *new_joined_path;
-    int counter;
-
-    path = getenv("PATH");
-    path_arr = ft_split(path, ':');
-
-    counter = 0;
-    while (path_arr[counter])
-    {
-        joined_path = ft_strjoin(path_arr[counter], "/");
-        new_joined_path = ft_strjoin(joined_path, cmd);
-        safe_free(joined_path);
-        if (access(new_joined_path, F_OK) == 0)
-        {
-            safe_free(path_arr);
-            return (new_joined_path);
-        }
-        counter++;
-    }
-    return (NULL);
-}
-
-int isbuiltin(char *cmd)
-{
-    if (ft_strncmp(cmd, "echo", ft_strlen("echo")) == 0)
-        return (1);
-    if (ft_strncmp(cmd, "export", ft_strlen("export")) == 0)
-        return (1);
-    if (ft_strncmp(cmd, "env", ft_strlen("env")) == 0)
-        return(1);
-    if (ft_strncmp(cmd, "pwd", ft_strlen("pwd")) == 0)
-        return (1);
-    if (ft_strncmp(cmd, "cd", ft_strlen("cd")) == 0)
-        return(1);
-    if (ft_strncmp(cmd, "unset", ft_strlen("unset")) == 0)
-        return(1);
-    return (0);
-}
 void execute(t_command *cmds, t_command *beginCmds)
 {
     int fd[2];
@@ -98,106 +69,52 @@ void execute(t_command *cmds, t_command *beginCmds)
     char **arg;
     t_command *before;
     int builtin;
-    char *line;
 
     builtin = isbuiltin(cmds->name);
-    char buff[512];
-    int rd_bytes;
-    char **arg;
-    t_command *before;
-    int builtin;
-
-    builtin = isbuiltin(cmds->name);
-
-    //echo builtin
-    if (!builtin)
-    {
-        path_cmd = find_path(cmds->name);
-        if (!path_cmd)
-        {
-            ft_putstr_fd("command not found: ", 2);
-            return ;
-        }
-        arg = new_arr(cmds->name, cmds->args);
-    }
     before = find_before_cmd(cmds, beginCmds);
-
-    if (!before)
-    {
-        if (cmds->next == NULL)
-            before = NULL;
-    }
+    
     if (pipe(fd) == -1)
-    {
-        ft_putstr_fd("Pipe error!", 2);
-        return;
-    }
+        return ft_putstr_fd("Pipe error!", 2);
+
     if (!builtin)
     {
         pid = fork();
         if (pid == -1)
-        {
-            ft_putstr_fd("Fork error!", 2);
-            return;
-        }
-        if (pid == 0)
-        if (pid == 0) // child process
+            return ft_putstr_fd("Fork error!", 2);
+        
+        path_cmd = find_path(cmds->name);
+        if (!path_cmd)
+            return ft_putstr_fd("command not found: ", 2);
+        arg = new_arr(cmds->name, cmds->args);
+
+        if (pid == 0) //child
         {
             if (before)
             {
                 dup2(before->fd, STDIN_FILENO);
                 close(before->fd);
             }
-            close (fd[0]);
+            close(fd[0]);
             dup2(fd[1], STDOUT_FILENO);
-            close (fd[1]);
+            close(fd[1]);
             execve(path_cmd, arg, NULL);
         }
-        waitpid(pid, NULL, 0);
+        //waitpid(pid, NULL, 0);
         safe_free(arg);
-        close(fd[1]);
     }
     else
-    {
-        if (ft_strncmp(cmds->name, "echo", ft_strlen("echo")) == 0)
-        {
-            builtin_echo(cmds);
-            write(fd[1], cmds->args[0], ft_strlen(cmds->args[0]));
-        }
-        else if (ft_strncmp(cmds->name, "export", ft_strlen("export")) == 0)
-        {
-            t_envList *tmp = envList;
-            while (tmp)
-            {
-                write(fd[1], tmp->key, ft_strlen(tmp->key));
-                write(fd[1], "=", 1);
-                write(fd[1], tmp->value, ft_strlen(tmp->value));
-                write(fd[1], "\n", 1);
-                tmp = tmp->next;
-            }
-        }
-        else if (ft_strncmp(cmds->name, "env", ft_strlen("env")) == 0) {
-            print_list(envList);
-        }
-    }
-        }
-        else if (ft_strncmp(cmds->name, "env", ft_strlen("env")) == 0)
-            print_list(envList);
-
+        handle_builtin(cmds, fd);
     close(fd[1]);
     if (before)
        close(before->fd);
-
     if (cmds->next)
+    {
         cmds->fd = fd[0];
-    else
-    {   
-        while ((line = get_next_line(fd[0])) != NULL)
-        {
-            write(1, line, ft_strlen(line));
-            safe_free(line);
-        }
-        close(fd[0]);
-    if (cmds->next)
         execute(cmds->next, beginCmds);
+    }
+    else
+    {
+        read_and_print(fd[0]);
+        close(fd[0]);
+    }
 }
