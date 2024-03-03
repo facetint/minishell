6284c9ae53519f53144140a086e0a9c52c4607c5
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-03 14:24:49 +0300

    feat: add to parametres in the handle_builtin function

--- builtin/builtin.c
+++ builtin/builtin.c
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 13:04:26 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/03 13:04:27 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "../includes/minishell.h"
 #include "../libft/libft.h"
 #include "../memory-allocator/allocator.h"
+#include "../includes/env.h"
 
 
 int isbuiltin(char *cmd)
@@ -23,18 +36,18 @@ int isbuiltin(char *cmd)
     return (0);
 }
 
-void    handle_builtin(t_command *cmd)
+void    handle_builtin(t_command *cmd, int fd[2])
 {
     if (ft_strncmp(cmd->name, "echo", ft_strlen("echo")) == 0)
-        return;
-    if (ft_strncmp(cmd->name, "export", ft_strlen("export")) == 0)
-        return;
-    if (ft_strncmp(cmd->name, "env", ft_strlen("env")) == 0)
-        return;
-    if (ft_strncmp(cmd->name, "exit", ft_strlen("exit")) == 0)
-        return;
-    if (ft_strncmp(cmd->name, "pwd", ft_strlen("pwd")) == 0)
-        return;
+        builtin_echo(cmd, fd);
+    else if (ft_strncmp(cmd->name, "export", ft_strlen("export")) == 0)
+        builtin_export(cmd, fd);
+    else if (ft_strncmp(cmd->name, "env", ft_strlen("env")) == 0)
+        builtin_env(get_global_env());
+    else if (ft_strncmp(cmd->name, "exit", ft_strlen("exit")) == 0)
+        builtin_exit(cmd);
+    else if (ft_strncmp(cmd->name, "pwd", ft_strlen("pwd")) == 0)
+        builtin_pwd(cmd);
     if (ft_strncmp(cmd->name, "cd", ft_strlen("cd")) == 0)
         return;
     if (ft_strncmp(cmd->name, "unset", ft_strlen("unset")) == 0)
