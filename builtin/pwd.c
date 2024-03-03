bcd56ec4076d10a5e84a0ccf3ba522171ba9da33
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-03 18:02:21 +0300

    let me sleep

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -3,25 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hcoskun <hcoskun@student.42.fr>            +#+  +:+       +#+        */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD:builtin/pwd.c
 /*   Created: 2024/03/02 13:41:06 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/03 13:17:22 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/03 18:01:51 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../libft/libft.h"
 #include <stdio.h>
-=======
-/*   Created: 2023/06/24 20:30:45 by hcoskun42         #+#    #+#             */
-/*   Updated: 2023/12/31 15:40:54 by hcoskun42        ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
->>>>>>> main:libft/ft_calloc.c
 #include "../memory-allocator/allocator.h"
 
 void    builtin_pwd(t_command *cmd)
