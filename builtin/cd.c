b2853d9c6db68a41eea4b96ead5baca79244fb6b
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-08 17:26:54 +0300

    feat: add the cd command

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,11 +6,99 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 11:48:16 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/03 17:58:08 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/08 17:02:53 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../libft/libft.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "../memory-allocator/allocator.h"
+#include "../includes/env.h"
 
+char    *get_home(t_command *cmd)
+{
+    (void)cmd;
+    t_envList   *env;
+    
+    env = get_global_env();
+    while (env)
+    {
+        if (!ft_strcmp(env->key, "HOME"))
+            return (env->value);
+        env = env->next;
+    }
+    return (NULL);
+}
+void    change_old(char *str, t_command *cmd)
+{
+    (void)cmd;
+    t_envList   *env;
+    env = get_global_env();
+    while (env)
+    {
+        if (!ft_strcmp(env->key, "OLDPWD"))
+        {
+            if (env->value)
+              safe_free(env->value);
+            env->value = str;
+            break;
+        }
+        env = env->next;
+    }
+}
+void    change_pwd(t_command *data, t_command *cmd)
+{
+    t_envList   *env;
+
+    env = get_global_env();
+    while (env)
+    {
+        if (!ft_strcmp(env->key, "PWD") && data->args[0])
+        {
+            if (env->value)
+                free(env->value);
+            env->value = get_home(cmd);
+        }
+        else if (!ft_strcmp(env->key, "PWD"))
+        {
+            if (env->value)
+              safe_free(env->value);
+            env->value = safe_malloc(sizeof(char) * 4097);
+            getcwd(env->value, 4097);
+            break;
+        }
+        env = env->next;
+    }
+}
+void    execute_cd(char *str, t_command *data, t_command *cmd)
+{
+    chdir(get_home(cmd));
+    change_old(str, cmd);
+    change_pwd(data, cmd);
+}
+void    builtin_cd(t_command *data, t_command *cmd)
+{
+    char    *str;
+
+    str = safe_malloc(sizeof(char) * 4097);
+    getcwd(str, 4097);
+    if (data->args[0] && data->args)
+    {
+        if (chdir(data->args[0]) == 0)
+        {
+            change_old(str, cmd);
+            change_pwd(data, cmd);
+        }
+        else
+        {
+            if (str)
+                free(str);
+            perror("cd");
+        }
+    }
+    else
+        execute_cd(str, data, cmd);
+}
