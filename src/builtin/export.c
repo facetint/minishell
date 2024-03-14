0e2a89ba5ffe97e8edf2888c30c92c6be52b8c1e
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-14 18:51:13 +0300

    chore: import to the newly add folder

---
+++ src/builtin/export.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 18:01:40 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/14 16:05:55 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+#include "../../includes/env.h"
+#include <unistd.h>
+#include "../../libft/libft.h"
+
+void    builtin_export(t_command *cmd, int fd[2])
+{
+    (void)cmd;
+    t_envList *tmp = get_global_env();
+    while (tmp)
+    {
+        write(fd[1], "declare -x ", 12);
+        write(fd[1], tmp->key, ft_strlen(tmp->key));
+        write(fd[1], "=", 1);
+        write(fd[1], tmp->value, ft_strlen(tmp->value));
+        write(fd[1], "\n", 1);
+        tmp = tmp->next;
+    }
+}
---
+++ src/env/env_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 12:40:58 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/14 17:55:54 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+#include "../../includes/env.h"
+#include <stdlib.h>
+#include "../../memory-allocator/allocator.h"
+
+char	*strchr_new(const char *s, int c)
+{
+	while ((char)c != *s)
+	{
+		if (!*s)
+			return (0);
+		s++;
+	}
+	return ((char *)s + 1);
+}
+char    *strdup_n(char *str, char c)
+{
+    size_t  i;
+    char *ret_val;
+
+    if (!str || str[0] == 0)
+        return (0);
+    i = 0;
+    while (str[i] && str[i] != c)
+        i++;
+    ret_val = safe_malloc(sizeof(char) * (i + 1));
+    ret_val[i] = 0;
+    i = 0;
+    while (str[i] && str[i] != c)
+    {
+        ret_val[i] = str[i];
+        i++;
+    }
+    return (ret_val);
+}
+int lst_size(t_envList *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		count++;
+	}
+	return (count);
+}
+int ft_strcmp(char *s1, char *s2)
+{
+    if (!s1 || !s2)
+        return (1);
+	int	counter;
+
+	counter = 0;
+	while (s1[counter] && s1[counter] == s2[counter])
+		counter++;
+	return (s1[counter] - s2[counter]);
+}
