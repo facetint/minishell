993b0eda443467d95d88b84b5cfd166d2ad5d898
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-14 18:46:24 +0300

    fix: fix command name problem in handle_builtin function

---
+++ src/builtin/builtin.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 13:04:26 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/14 16:06:38 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../../includes/minishell.h"
+#include "../../libft/libft.h"
+#include "../../memory-allocator/allocator.h"
+#include "../../includes/env.h"
+
+
+int isbuiltin(char *cmd)
+{
+    if (ft_strcmp(cmd, "echo") == 0)
+        return (1);
+    if (ft_strcmp(cmd, "export") == 0)
+        return (1);
+    if (ft_strcmp(cmd, "env") == 0)
+        return(1);
+    if (ft_strcmp(cmd, "exit") == 0)
+        return (1);
+    if (ft_strcmp(cmd, "pwd") == 0)
+        return (1);
+    if (ft_strcmp(cmd, "cd") == 0)
+        return(1);
+    if (ft_strcmp(cmd, "unset") == 0)
+        return(1);
+    return (0);
+}
+void    handle_builtin(t_command *cmd, int fd[2])
+{
+    if (ft_strcmp(cmd->args[0], "echo") == 0)
+        builtin_echo(cmd, fd);
+    else if (ft_strcmp(cmd->args[0], "export") == 0)
+        builtin_export(cmd, fd);
+    else if (ft_strcmp(cmd->args[0], "env") == 0)
+        builtin_env(get_global_env(), fd);
+    else if (ft_strcmp(cmd->args[0], "exit") == 0)
+        builtin_exit(cmd);
+    else if (ft_strcmp(cmd->args[0], "pwd") == 0)
+        builtin_pwd(cmd);
+    if (ft_strcmp(cmd->args[0], "cd") == 0)
+        builtin_cd(cmd, cmd);
+    if (ft_strcmp(cmd->args[0], "unset") == 0)
+        return;
+}
