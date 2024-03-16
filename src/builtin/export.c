5bf21a68323b0c3c9e85571f2c258d4e1d1e65a5
Hamza Coskun <hcoskun@k2m18s06.42kocaeli.com.tr>
2024-03-16 14:49:52 +0300

    chore: use putstr instead of write

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -3,33 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hcoskun <hcoskun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 18:01:40 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/14 16:05:55 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/16 14:15:38 by hcoskun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "../../includes/env.h"
+#include "../../includes/utils.h"
 #include <unistd.h>
 #include "../../libft/libft.h"
 
 void    builtin_export(t_command *cmd, int fd[2])
 {
     t_list *tmp;
-    t_envList *node;
+    t_entry *node;
+	int args_count;
+	int i;
+	
     (void)cmd;
+	args_count = str_arr_size(cmd->args);
+	if (args_count == 1)
+	{
+		tmp = get_global_env();
+		while (tmp)
+		{
+			node = tmp->content;
+			ft_putstr_fd("declare -x ", fd[1]);
+			ft_putstr_fd(node->key, fd[1]);
+			ft_putstr_fd("=", fd[1]);
+			ft_putstr_fd(node->value, fd[1]);
+			ft_putstr_fd("\n", fd[1]);
+			tmp = tmp->next;
+		}
+		return;
+	}
 
-    tmp = get_global_env();
-    while (tmp)
-    {
-        node = tmp->content;
-        write(fd[1], "declare -x ", 12);
-        write(fd[1], node->key, ft_strlen(node->key));
-        write(fd[1], "=", 1);
-        write(fd[1], node->value, ft_strlen(node->value));
-        write(fd[1], "\n", 1);
-        tmp = tmp->next;
-    }
+	i = 1;
+	while (cmd->args[1]) {
+		
+	}
 }
