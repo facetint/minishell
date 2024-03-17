de6f0a5de7a52af4eb6d240e094e46347d58c6fe
hamza <hamzacoskun41@gmail.com>
2024-03-17 14:09:01 +0300

    fix: compilation error

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
