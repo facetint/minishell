66bd2f63104e9eb087911ccd73dae1deaca7541c
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 15:05:08 +0300

    sytle: edit the norminette

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,24 +6,24 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 20:06:35 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/25 15:12:25 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/28 14:56:11 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/libft.h"
 #include "../../includes/env.h"
 
-void    builtin_env(t_list *env, int fd[2])
+void	builtin_env(t_list *env, int fd[2])
 {
-    t_entry   *node;
+	t_entry	*node;
 
-    while (env)
-    {
-        node = env->content;
-        ft_putstr_fd(node->key, fd[1]);
-        ft_putstr_fd("=", fd[1]);
-        ft_putstr_fd(node->value, fd[1]);
-        ft_putstr_fd("\n", fd[1]);
-        env = env->next;
-    }
+	while (env)
+	{
+		node = env->content;
+		ft_putstr_fd(node->key, fd[1]);
+		ft_putstr_fd("=", fd[1]);
+		ft_putstr_fd(node->value, fd[1]);
+		ft_putstr_fd("\n", fd[1]);
+		env = env->next;
+	}
 }
