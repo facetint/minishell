aff28f76d0bc9f8e70c0abfd7ea1dad260fde6ec
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 15:05:17 +0300

    sytle: edit the norminette

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:19:04 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/26 11:15:34 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/28 15:04:28 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 #include "../../libft/libft.h"
 #include "../../includes/env.h"
 
-void    builtin_unset(t_command *cmd, int fd[2])
+void	builtin_unset(t_command *cmd, int fd[2])
 {
 	(void)fd;
 	unset_env(cmd->args[1]);
 }
 
-void unset_env(char *varname)
+void	unset_env(char *varname)
 {
-	t_list *cur;
-	t_list *prev;
+	t_list	*cur;
+	t_list	*prev;
 
 	cur = *get_global_env();
 	prev = NULL;
@@ -34,13 +34,12 @@ void unset_env(char *varname)
 		{
 			if (prev)
 				prev->next = cur->next;
-			else 
 			*get_global_env() = cur->next;
 			free(((t_entry *)cur->content)->key);
 			free(((t_entry *)cur->content)->value);
 			free(cur->content);
 			free(cur);
-			return;
+			return ;
 		}
 		prev = cur;
 		cur = cur->next;
