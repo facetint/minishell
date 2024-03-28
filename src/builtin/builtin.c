222e6f2b0fcd4581c73db42559f69da2db059143
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 14:54:23 +0300

    style: edit the norminette

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:04:26 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/25 14:58:45 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/28 12:55:29 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,39 @@
 #include "../../memory-allocator/allocator.h"
 #include "../../includes/env.h"
 
-int isbuiltin(char *cmd)
+int	isbuiltin(char *cmd)
 {
-    if (ft_strcmp(cmd, "echo") == 0)
-        return (1);
-    if (ft_strcmp(cmd, "export") == 0)
-        return (1);
-    if (ft_strcmp(cmd, "env") == 0)
-        return(1);
-    if (ft_strcmp(cmd, "exit") == 0)
-        return (1);
-    if (ft_strcmp(cmd, "pwd") == 0)
-        return (1);
-    if (ft_strcmp(cmd, "cd") == 0)
-        return(1);
-    if (ft_strcmp(cmd, "unset") == 0)
-        return(1);
-    return (0);
+	if (ft_strcmp(cmd, "echo") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "export") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "env") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "exit") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "pwd") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "cd") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "unset") == 0)
+		return (1);
+	return (0);
 }
-void    handle_builtin(t_command *cmd, int fd[2])
+
+void	handle_builtin(t_command *cmd, int fd[2])
 {
-    if (ft_strcmp(cmd->args[0], "echo") == 0)
-        builtin_echo(cmd, fd);
-    else if (ft_strcmp(cmd->args[0], "export") == 0)
-        builtin_export(cmd, fd);
-    else if (ft_strcmp(cmd->args[0], "env") == 0)
-        builtin_env(*get_global_env(), fd);
-    else if (ft_strcmp(cmd->args[0], "exit") == 0)
-        builtin_exit(cmd);
-    else if (ft_strcmp(cmd->args[0], "pwd") == 0)
-        builtin_pwd(cmd);
-    if (ft_strcmp(cmd->args[0], "cd") == 0)
-        builtin_cd(cmd);
-    if (ft_strcmp(cmd->args[0], "unset") == 0)
+	if (ft_strcmp(cmd->args[0], "echo") == 0)
+		builtin_echo(cmd, fd);
+	else if (ft_strcmp(cmd->args[0], "export") == 0)
+		builtin_export(cmd, fd);
+	else if (ft_strcmp(cmd->args[0], "env") == 0)
+		builtin_env(*get_global_env(), fd);
+	else if (ft_strcmp(cmd->args[0], "exit") == 0)
+		builtin_exit(cmd);
+	else if (ft_strcmp(cmd->args[0], "pwd") == 0)
+		builtin_pwd(cmd);
+	else if (ft_strcmp(cmd->args[0], "cd") == 0)
+		builtin_cd(cmd);
+	else if (ft_strcmp(cmd->args[0], "unset") == 0)
 		builtin_unset(cmd, fd);
 }
