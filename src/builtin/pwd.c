dba12501be3c336554d0fedf4fee02af1b169f1d
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 16:31:05 +0300

    style: edit the norminette

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:41:06 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/14 16:06:25 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/28 16:30:51 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 #include <stdio.h>
 #include "../../memory-allocator/allocator.h"
 
-void    builtin_pwd(t_command *cmd)
+void	builtin_pwd(t_command *cmd)
 {
-    (void)cmd;
-    char    *path;
-    
-    path = safe_malloc(sizeof(char) * 4097);
+	char	*path;
+
+	(void)cmd;
+	path = safe_malloc(sizeof(char) * 4097);
 	getcwd(path, 4097);
 	printf("%s\n", path);
 	safe_free(path);
