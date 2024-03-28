ec276c295bd85c3a084542129ad97bc7e853243f
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 17:01:45 +0300

    refactor: remove code description comment lines

--- src/expander/expander_nonvariables.c
+++ src/expander/expander_nonvariables.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:17:36 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/14 16:06:57 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:01:01 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,6 @@
 #include "../../memory-allocator/allocator.h"
 #include <stdio.h>
 
-/**
- * Removes the token from the list and inserts unquoted word tokens in its place.
- * 
- * Example:
- *                  k    l    m
- * assume we have: UW D UW D UW
- * and we call this function with the token 'l'
- * with the strings: {"a", "b", "c", "d"}
- * 
- *                                   a    b    c    d
- * 1. a token list will be created: UW D UW D UW D UW
- * 2. the token 'l' will be removed.
- * 3. the token list will be inserted in place of the removed token.
- *          k    a    b    c	d    m
- * Result: UW D UW D UW D UW D UW D UW
- *              ~~~~~~~~~~~~~~~~~
-*/
 void insert_uword_tokens(t_token **token_ptr, char **strings)
 {
 	t_token *token;
