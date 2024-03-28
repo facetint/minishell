a9e722ded2514e3c13974dc2fe8e6c01f58b76f2
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-28 16:00:47 +0300

    style: edit the norminette

--- src/execute/heredoc.c
+++ src/execute/heredoc.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 15:58:39 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/28 16:00:10 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "../../includes/minishell.h"
 #include "../../libft/libft.h"
 #include <readline/readline.h>
 
-void heredoc_to_fd(char *delimiter, int output_fd)
+void	heredoc_to_fd(char *delimiter, int output_fd)
 {
-	char *line;
+	char	*line;
 
 	while (1)
 	{
 		line = readline("> ");
 		if (!line || ft_strncmp(line, delimiter, INT_MAX) == 0)
-			break;
+			break ;
 		if (!*line)
 			line = ft_strdup("\n");
 		else
@@ -22,30 +34,31 @@ void heredoc_to_fd(char *delimiter, int output_fd)
 	}
 }
 
-int read_heredoc_input(char *delimiter)
+int	read_heredoc_input(char *delimiter)
 {
-	int exit_status;
-	int pid;
-	int pipe_fd[2];
+	int	exit_status;
+	int	pid;
+	int	pipe_fd[2];
 
 	pipe(pipe_fd);
 	signal_type = WAITING_HEREDOC;
 	pid = fork();
 	if (pid < 0)
-		return -1;
+		return (-1);
 	if (pid == 0)
 	{
 		close(pipe_fd[0]);
 		signal_type = IN_HEREDOC;
 		heredoc_to_fd(delimiter, pipe_fd[1]);
 		exit(0);
-	} else {
+	}
+	else
+	{
 		close(pipe_fd[1]);
 		waitpid(pid, &exit_status, 0);
 		exit_status >>= 8;
 		if (exit_status != 0)
-			return -1;
-		return pipe_fd[0];
+			return (-1);
+		return (pipe_fd[0]);
 	}
 }
-
