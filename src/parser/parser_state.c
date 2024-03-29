d4222bbe3bcda7bcc737c1f59d2e2b6c07198249
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-29 17:44:41 +0300

    refactor: move to new file due to function redundancy

---
+++ src/parser/parser_state.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_state.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 17:34:28 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/29 17:37:32 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft/libft.h"
+#include "../../includes/minishell.h"
+#include "../../includes/utils.h"
+#include "../../memory-allocator/allocator.h"
+#include <stdio.h>
+
+void argument_state(t_token **cur_token, t_command *cur_cmd)
+{
+	int arg_index;
+
+	arg_index = str_arr_size(cur_cmd->args);
+	join_all_composed_words(cur_token, &cur_cmd->args[arg_index]);
+}
+
+void operator_state_p(t_token **cur_token, t_command *cur_cmd)
+{
+	t_token *lexer_data;
+	t_redirection redirection;
+	int last_index;
+
+	lexer_data = *cur_token;
+	if (lexer_data->type == PIPE)
+		return;
+
+	last_index = 0;
+	while (cur_cmd->redirections[last_index].redirected)
+		last_index++;
+
+	redirection = create_redirection_data(&lexer_data);
+	cur_cmd->redirections[last_index] = redirection;
+
+	*cur_token = lexer_data;
+}
+
+parser_state decide_next_state(t_token **cur_token)
+{
+    if (!*cur_token)
+         return NULL;
+   	if ((*cur_token)->type == DELIMITER)
+	   *cur_token = (*cur_token)->next;
+	if (is_word((*cur_token)->type))
+	   return (parser_state) argument_state;
+	else if (is_operator((*cur_token)->type))
+	   return (parser_state) operator_state_p;
+	return NULL;
+}
---
+++ src/parser/parser_utils.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 17:36:24 by facetint          #+#    #+#             */
+/*   Updated: 2024/03/29 17:37:43 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft/libft.h"
+#include "../../includes/minishell.h"
+#include "../../includes/utils.h"
+#include "../../memory-allocator/allocator.h"
+#include <stdio.h>
+
+void join_all_composed_words(t_token **cur_token, char **string_ptr)
+{
+	char *new_name;
+	t_token *lexer_data;
+
+	lexer_data = *cur_token;
+	if (lexer_data->type == DELIMITER)
+		lexer_data = lexer_data->next;
+	while (lexer_data && is_word(lexer_data->type))
+	{
+		new_name = ft_strjoin(*string_ptr, lexer_data->value);
+		if (*string_ptr)
+			safe_free(*string_ptr);
+		*string_ptr = new_name;
+		lexer_data = lexer_data->next;
+	}
+	*cur_token = lexer_data;
+}
+
+t_redirection create_redirection_data(t_token **lexer_data)
+{
+    t_redirection redirection;
+
+   	redirection = (t_redirection){0};
+	if ((*lexer_data)->type == INPUT_REDIRECTION)
+		redirection.flags = INPUT;
+	else if ((*lexer_data)->type == HEREDOC_REDIRECTION)
+		redirection.flags = HEREDOC | INPUT;
+	else if ((*lexer_data)->type == APPEND_REDIRECTION)
+		redirection.flags = APPEND;
+	*lexer_data = (*lexer_data)->next;
+	join_all_composed_words(lexer_data, &redirection.redirected);
+	return redirection;
+}
+
+int count_cmd_args(t_token *lexer_data) {
+    int arg_count;
+
+	arg_count = 0;
+	while (lexer_data && lexer_data->type != PIPE)
+	{
+		if (is_operator(lexer_data->type))
+		{
+			while (lexer_data && !is_word(lexer_data->type))
+				lexer_data = lexer_data->next;
+			if (lexer_data)
+				lexer_data = lexer_data->next;
+			continue;
+		}
+		if (lexer_data->type == DELIMITER)
+			lexer_data = lexer_data->next;
+		if (lexer_data && is_word(lexer_data->type))
+		{
+		    while (lexer_data && is_word(lexer_data->type))
+				lexer_data = lexer_data->next;
+			arg_count++;
+		}
+		if (lexer_data)
+		  lexer_data = lexer_data->next;
+	}
+	return arg_count;
+}
+
+int count_cmd_redirections(t_token *lexer_data)
+{
+	int redirection_count;
+
+	redirection_count = 0;
+	while (lexer_data && lexer_data->type != PIPE)
+	{
+		if (is_operator(lexer_data->type) && lexer_data->type != PIPE)
+			redirection_count++;
+		lexer_data = lexer_data->next;
+	}
+	return redirection_count;
+}
