e072f1e33504c7594e7fbd42b8de0d873ba2cd1a
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2024-03-29 16:50:17 +0300

    style: edit the norminette

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:17:50 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/14 16:07:31 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/29 16:46:05 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,44 @@
 #include "../../memory-allocator/allocator.h"
 #include <stdlib.h>
 
-t_token *get_last_lexer_data(t_token *token)
+t_token	*get_last_lexer_data(t_token *token)
 {
 	if (!token)
-		return NULL;
+		return (NULL);
 	while (token->next)
 		token = token->next;
-	return token;
+	return (token);
 }
 
-t_token *lexer_data_new(t_token token)
+t_token	*lexer_data_new(t_token token)
 {
-	t_token *data;
+	t_token	*data;
 
 	data = safe_malloc(sizeof(t_token));
 	*data = token;
 	data->next = NULL;
-	return data;
+	return (data);
 }
 
-void lexer_data_insert(t_token *data, t_token *new_list) {
-	t_token *last;
+void	lexer_data_insert(t_token *data, t_token *new_list)
+{
+	t_token	*last;
 
 	if (!data || !new_list)
-		return;
+		return ;
 	last = get_last_lexer_data(new_list);
 	last->next = data->next;
 	data->next = new_list;
 }
 
-void lexer_data_append(t_token **data, t_token *new_data)
+void	lexer_data_append(t_token **data, t_token *new_data)
 {
-	t_token *cur_data;
+	t_token	*cur_data;
 
 	if (*data == NULL)
 	{
 		*data = new_data;
-		return;
+		return ;
 	}
 	cur_data = *data;
 	while (cur_data->next != NULL)
@@ -58,8 +59,9 @@ void lexer_data_append(t_token **data, t_token *new_data)
 	cur_data->next = new_data;
 }
 
-void uninit_tokens(t_token *lexical_data) {
-	t_token *next;
+void	uninit_tokens(t_token *lexical_data)
+{
+	t_token	*next;
 
 	while (lexical_data)
 	{
--- src/lexer/unquote.c
+++ src/lexer/unquote.c
@@ -6,7 +6,7 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:18:41 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/28 16:57:58 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/29 16:49:39 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 #include "../../memory-allocator/allocator.h"
 #include "../../includes/utils.h"
 
-int count_escapes(char *str)
+int	count_escapes(char *str)
 {
-	unsigned int i;
-	int count;
+	int				count;
+	unsigned int	i;
 
 	i = 0;
 	count = 0;
@@ -29,15 +29,15 @@ int count_escapes(char *str)
 			count++;
 		i++;
 	}
-	return count;
+	return (count);
 }
 
-char *escaped_strdup(char *str)
+char	*escaped_strdup(char *str)
 {
-	char *result;
-	unsigned int i;
-	unsigned int j;
-	int length;
+	char			*result;
+	unsigned int	i;
+	unsigned int	j;
+	int				length;
 
 	length = (int) ft_strlen(str) - count_escapes(str);
 	result = safe_malloc(length + 1);
@@ -48,34 +48,36 @@ char *escaped_strdup(char *str)
 		if (str[i] == '\\')
 		{
 			if (str[i + 1] == '\0')
-				break;
+				break ;
 			i++;
 		}
 		result[j++] = str[i++];
 	}
 	result[j] = '\0';
-	return result;
+	return (result);
 }
 
 void	unquote(t_token *lexer_data)
 {
 	char	*unquoted_value;
+
 	while (lexer_data)
 	{
-		if (lexer_data->type == SINGLE_QUOTED_WORD || lexer_data->type == DOUBLE_QUOTED_WORD)
+		if (lexer_data->type == SINGLE_QUOTED_WORD
+			|| lexer_data->type == DOUBLE_QUOTED_WORD)
 		{
-			unquoted_value = ft_substr(lexer_data->value, 1, ft_strlen(lexer_data->value) - 2);
+			unquoted_value = ft_substr(lexer_data->value, 1,
+					ft_strlen(lexer_data->value) - 2);
 			safe_free(lexer_data->value);
 			lexer_data->value = unquoted_value;
 		}
-		if(lexer_data->type == UNQUOTED_WORD || lexer_data->type == DOUBLE_QUOTED_WORD)
+		if (lexer_data->type == UNQUOTED_WORD
+			|| lexer_data->type == DOUBLE_QUOTED_WORD)
 		{
 			unquoted_value = escaped_strdup(lexer_data->value);
 			safe_free(lexer_data->value);
 			lexer_data->value = unquoted_value;
 		}
-
 		lexer_data = lexer_data->next;
 	}
-
 }
