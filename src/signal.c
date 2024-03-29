8e9cd2969fa26121489520f8a1f3acec3a32572d
hamza <hamzacoskun41@gmail.com>
2024-03-29 23:09:10 +0300

    chore: uncomment rl_on_new_line

--- src/signal.c
+++ src/signal.c
@@ -13,17 +13,17 @@ void	handle_signal(int signum)
 	*get_exit_status() = 1;
 	if (signal_type == PROMPT) {
 		printf("%s%s  \n", get_prompt(), rl_line_buffer);
-		//rl_replace_line("", 0);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
 	} else if (signal_type == RUNNING_COMMANDS) {
 		printf("\n");
-		//rl_replace_line("", 0);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		*get_exit_status() = 128 + signum;
 	} else if (signal_type == IN_HEREDOC) {
 		printf("> %s  \n", rl_line_buffer);
-		//rl_replace_line("", 0);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		exit(1);
 	}
