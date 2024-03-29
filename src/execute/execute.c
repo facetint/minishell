67eb839a1aa65f2663535488b7963d4353cf8ab8
hamza <hamzacoskun41@gmail.com>
2024-03-29 23:10:28 +0300

    fix: wrong redirections for builtins.

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hamza <hamza@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:34:07 by facetint          #+#    #+#             */
-/*   Updated: 2024/03/28 17:44:54 by facetint         ###   ########.fr       */
+/*   Updated: 2024/03/29 23:07:57 by hamza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,12 @@
 #include "../../includes/minishell.h"
 #include "../../includes/env.h"
 #include "../../memory-allocator/allocator.h"
+#include <stdio.h>
 
 int	should_run_in_child(t_command *cmd)
 {
-	return (cmd->prev || cmd->next || !isbuiltin(cmd->args[0]));
-}
-
-void	handle_command(int inp_fd, int out_fd, t_command *cmd, int *prev_p, int *next_p)
-{
-	int		pid;
-	char	*path_cmd;
-
-	if (inp_fd == -1 || out_fd == -1)
-		return ;
-	pid = 0;
-	if (should_run_in_child(cmd))
-		pid = fork();
-	if (pid < 0)
-		pid_error(pid, prev_p, next_p);
-	if (pid > 0)
-	{
-		cmd->pid = pid;
-		return ;
-	}
-	dup2_and_close(inp_fd, out_fd, prev_p, next_p);
-	path_cmd = find_path(cmd->args[0]);
-	if (!path_cmd)
-		path_error(cmd);
-	execve(path_cmd, cmd->args, to_arr(*get_global_env()));
-	exit(127);
+	int result = cmd->prev || cmd->next || !isbuiltin(cmd->args[0]);
+	return (result);
 }
 
 int	get_input_fd(int *pipe, t_command *cmd)
@@ -64,29 +41,66 @@ int	get_output_fd(int *pipe, t_command *cmd)
 	return (pipe[1]);
 }
 
-void	execute(t_command *cmds)
+void	handle_command(t_command *cmd, int *prev_p, int *next_p)
 {
-	t_command	*cur;
+	int		pid;
+	char	*path_cmd;
+	int		inp_fd;
+	int		out_fd;
+
+	inp_fd = get_input_fd(prev_p, cmd);
+	out_fd = get_output_fd(next_p, cmd);
+	if (inp_fd == -1 || out_fd == -1)
+		return ;
+	pid = 0;
+	if (should_run_in_child(cmd))
+		pid = fork();
+	if (pid < 0)
+		pid_error(pid, prev_p, next_p);
+	if (pid > 0)
+	{
+		cmd->pid = pid;
+		return ;
+	}
+	if (isbuiltin(cmd->args[0]))
+	{
+		handle_builtin(cmd, (int[]){inp_fd, out_fd});
+		if (should_run_in_child(cmd))
+			exit(0);
+		return;
+	}
+	dup2_and_close(inp_fd, out_fd, prev_p, next_p);
+	path_cmd = find_path(cmd->args[0]);
+	if (!path_cmd)
+		path_error(cmd);
+	execve(path_cmd, cmd->args, to_arr(*get_global_env()));
+	exit(127);
+}
+
+void close_pipe(int *pipe)
+{
+	if (pipe)
+	{
+		close(pipe[0]);
+		close(pipe[1]);
+	}
+}
+
+void	execute(t_command *cur)
+{
 	t_command	*latest;
 	int			exit_status;
 	int			*prev_p;
 	int			*next_p;
 
-	if (!cmds->next && isbuiltin(cmds->args[0]) && cmds->args[0])
-		return (handle_builtin (cmds, (int [])
-				{STDIN_FILENO, STDOUT_FILENO}));
 	*get_exit_status() = 0;
+		
 	prev_p = NULL;
 	next_p = NULL;
-	cur = cmds;
 	latest = NULL;
 	while (cur)
 	{
-		if (prev_p)
-		{
-			close(prev_p[0]);
-			close(prev_p[1]);
-		}
+		close_pipe(prev_p);
 		prev_p = next_p;
 		if (cur->next)
 		{
@@ -97,15 +111,11 @@ void	execute(t_command *cmds)
 		{
 			next_p = NULL;
 		}
-		handle_command(get_input_fd(prev_p, cur), get_output_fd(next_p, cur), cur, prev_p, next_p);
+		handle_command(cur, prev_p, next_p);
 		latest = cur;
 		cur = cur->next;
 	}
-	if (prev_p)
-	{
-		close(prev_p[0]);
-		close(prev_p[1]);
-	}
+	close_pipe(prev_p);
 	if (latest->pid == 0)
 	{
 		*get_exit_status() = 1;
